966511abd17c9ea022b22166ef7c05e6ba77c56e
Ricky Roy Nardson <rickyroynardson@gmail.com>
2022-09-01 12:15:11 +0700

    styling login page and header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { useAuthContext } from "../hooks/useAuthContext";
 import { useLogout } from "../hooks/useLogout";
@@ -6,37 +7,50 @@ const Navbar = () => {
   const { user } = useAuthContext();
   const { logout } = useLogout();
   const location = useLocation();
+  const [isOpen, setIsOpen] = useState(false);
 
   const isActive = (path) => {
     return location.pathname === path;
   };
 
   const handleLogout = () => {
+    setIsOpen(false);
     logout();
   };
 
   return (
-    <header className="bg-white shadow p-4">
-      <div className="flex items-center justify-between">
-        <h1 className="text-xl font-medium">
-          <Link to="/">
-            <span className="border-b-2 border-sky-600 py-1">Workou</span>t
+    <header className='bg-white shadow p-4'>
+      <div className='flex items-center justify-between'>
+        <h1 className='text-xl font-medium'>
+          <Link to='/'>
+            <span className='border-b-2 border-sky-600 py-1'>Workou</span>t
             Buddy
           </Link>
         </h1>
         <nav>
           {user ? (
-            <ul className="flex items-center list-none gap-3">
-              <li>{user.email}</li>
-              <li>
-                <button onClick={handleLogout}>Logout</button>
-              </li>
-            </ul>
+            <div className='relative'>
+              <button
+                className='py-1 text-sm'
+                onClick={() => setIsOpen(!isOpen)}
+              >
+                {user.email}
+              </button>
+              <ul
+                className={`absolute right-0 mt-2 w-full rounded-md overflow-hidden bg-white shadow ${
+                  isOpen ? "visible" : "invisible"
+                }`}
+              >
+                <li className='text-sm px-3 py-1.5 hover:bg-slate-50'>
+                  <button onClick={handleLogout}>Logout</button>
+                </li>
+              </ul>
+            </div>
           ) : (
-            <ul className="flex items-center list-none gap-3">
+            <ul className='flex items-center list-none gap-3'>
               <li>
                 <Link
-                  to="/login"
+                  to='/login'
                   className={`py-1 border-b-2  hover:border-sky-600 transition-all duration-200 ${
                     isActive("/login") ? "border-sky-600" : "border-transparent"
                   }`}
@@ -46,7 +60,7 @@ const Navbar = () => {
               </li>
               <li>
                 <Link
-                  to="/register"
+                  to='/register'
                   className={`py-1 border-b-2 hover:border-sky-600 transition-all duration-200 ${
                     isActive("/register")
                       ? "border-sky-600"
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import { useLogin } from "../hooks/useLogin";
 
 const Login = () => {
@@ -12,27 +13,47 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <h3>Login</h3>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit" disabled={isLoading}>
+    <div className='bg-white shadow rounded-xl m-6 p-4'>
+      <h3 className='text-xl font-semibold text-sky-600'>Login</h3>
+      {error && (
+        <ul className='my-3 text-red-600 list-disc list-inside'>
+          <li>{error}</li>
+        </ul>
+      )}
+      <form className='my-3' onSubmit={handleSubmit}>
+        <div className='my-2'>
+          <label htmlFor='email'>Email</label>
+          <input
+            type='email'
+            id='email'
+            className='block w-full px-3 py-1.5 border rounded-xl text-gray-600 focus:outline-none focus:ring-1 focus:ring-sky-600 focus:shadow'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className='mb-5'>
+          <label htmlFor='password'>Password</label>
+          <input
+            type='password'
+            id='password'
+            className='block w-full px-3 py-1.5 border rounded-xl text-gray-600 focus:outline-none focus:ring-1 focus:ring-sky-600 focus:shadow'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button
+          type='submit'
+          disabled={isLoading}
+          className='bg-blue-500 w-full px-2 py-2 text-sm font-medium text-white rounded-xl transition-all duration-200 hover:bg-blue-600 disabled:bg-blue-800 focus:bg-blue-600 focus:outline-none focus:ring-2'
+        >
           Login
         </button>
-        {error && <div>{error}</div>}
+        <p className='text-sm my-1 text-gray-600'>
+          Don't have an account?{" "}
+          <Link to='/register' className='font-medium text-blue-500'>
+            Register
+          </Link>
+        </p>
       </form>
     </div>
   );
